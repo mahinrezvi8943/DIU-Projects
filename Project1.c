d9850031f358ccdb874c0f07221ca0726bdd9986
Syed Mahin Hossain Rezvi <146018653+mahinrezvi8943@users.noreply.github.com>
2023-11-05 00:59:45 +0600

    Project1.c

--- Project1.c
+++ Project1.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include<stdlib.h>
-
 struct Product {
     int id;
     char name[50];
@@ -17,25 +15,24 @@ struct Product products[] = {
     {6, "Ice Cream", 50, 15}
 };
 
-void menu()
-{
+void menu() {
     printf("\n           ╻━━━━━━━━━━━━━━━━━━━━━━━━╻        \n");
     printf("           ┃ Welcome to vending app ┃    \n");
     printf("           ╹━━━━━━━━━━━━━━━━━━━━━━━━╹        \n");
     printf("╻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╻\n");
-    printf("┃ %-6s %-15s %-12s %-2s ┃\n","ID", "Name", "Price", "Quantity");
+    printf("┃ %-6s %-15s %-12s %-2s ┃\n", "ID", "Name", "Price", "Quantity");
     
     printf("┃━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┃\n");
-    for(int i = 0; i < 6; i++)
-    {
-        printf("┃  %-5d %-15s %-12d %-2d      ┃\n", products[i].id, products[i].name, products[i].price, products[i].quantity);
+    for (int i = 0; i < 6; i++) {
+        printf("┃  %-5d %-15s %-12.2f %-2d       ┃\n", products[i].id, products[i].name, products[i].price, products[i].quantity);
     }
-    printf("┃▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁┃\n");
+    printf("┃▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁┃\n\n");
     printf("\n");
 }
-void select(int productID, int quantity, float* balance, float* totalCost) {
+
+void SELECT(int productID, int quantity, float* balance, float* totalCost) {
     if (productID < 1 || productID > 6) {
-        printf("Invalid selection. Please enter a valid product ID.\n");
+        printf("Invalid SELECTion. Please enter a valid product ID.\n");
         return;
     }
     int n = productID - 1;
@@ -45,7 +42,7 @@ void select(int productID, int quantity, float* balance, float* totalCost) {
     }
 
     float cost = products[n].price * quantity;
-    if ( cost > *balance) {
+    if (cost > *balance) {
         printf("Insufficient balance. Please insert more money.\n");
         return;
     }
@@ -62,28 +59,31 @@ void select(int productID, int quantity, float* balance, float* totalCost) {
 
 int main() {
     float balance = 100.0, totalCost = 0;
-    int productID, quantity, productCount = 0;
+    int productCount = 0;
     int Quantity[6];
     for (int i = 0; i < 6; i++) Quantity[i] = products[i].quantity;
-
+    
     while (1) {
         menu();
         printf("Current Balance: $%.2f\n", balance);
+        int productID, quantity;
+
         printf("Enter the product ID or 0 to exit: ");
         scanf("%d", &productID);
 
         if (productID == 0) break;
+
         printf("Enter the quantity you want to purchase: ");
         scanf("%d", &quantity);
 
-        select(productID, quantity, &balance, &totalCost);
+        SELECT(productID, quantity, &balance, &totalCost);
 
         int n = productID - 1;
 
         if (products[n].quantity < Quantity[n]) {
             int purchasedQuantity = Quantity[n] - products[n].quantity;
             float productTotalCost = purchasedQuantity * products[n].price;
-            printf("Added to Shopping Cart: %-2d %s for %.2fTk\n", purchasedQuantity, products[n].name, productTotalCost);
+            printf("Added to Shopping Cart: %-2d %s for $%.2f\n", purchasedQuantity, products[n].name, productTotalCost);
             productCount++;
         }
     }
@@ -91,7 +91,6 @@ int main() {
     printf("        ............┃ Shopping Cart ┃............\n");
 
     printf("                    ╹━━━━━━━━━━━━━━━╹\n\n");
-
     if (productCount > 0) {
         printf("╻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╻\n");
         printf("┃ %-5s %-20s %-12s %-12s ┃\n", "Qty", "Product", "Price(Tk.)", "Total");
@@ -99,12 +98,15 @@ int main() {
         for (int i = 0; i < 6; i++) {
             int quantity = Quantity[i] - products[i].quantity;
             float productTotalCost = quantity * products[i].price;
-            printf("┃  %-4d %-20s %-12.2f %-12.2f ┃\n", quantity, products[i].name, products[i].price, productTotalCost);
+                printf("┃  %-4d %-20s %-12.2f %-12.2f ┃\n", quantity, products[i].name, products[i].price, productTotalCost);
         }
         printf("╹━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╹\n");
-        printf("                            Total cost: $%.2f\n", totalCost);
-    } else printf("Your cart is empty.\n\n");
-    printf("\nThank you for using the Vending Machine! Your remaining balance is: $%.2f\n\n\n", balance);
+        printf("                            Total cost: %.2f Tk.\n", totalCost);
+    } else {
+        printf("Your cart is empty.\n\n");
+    }
+
+    printf("\nThank you for using the Vending Machine! Your remaining balance is: %.2f Tk.\n\n\n", balance);
 
     return 0;
 }
